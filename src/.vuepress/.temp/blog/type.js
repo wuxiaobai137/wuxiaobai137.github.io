d260795dc7f6fdd1374ba67183bacb4a9a7989eb
wucheng <wucheng.wu@alibaba-inc.com>
2024-06-04 22:34:11 +0800

    build

--- src/.vuepress/.temp/blog/type.js
+++ src/.vuepress/.temp/blog/type.js
@@ -1,14 +1,3 @@
-      export const typesMap = {"article":{"/":{"path":"/article/","indexes":[0,6,7,8,1,2,3,5,4]}},"star":{"/":{"path":"/star/","indexes":[0]}},"timeline":{"/":{"path":"/timeline/","indexes":[0]}}};
+      export const typesMap = {"article":{"/":{"path":"/article/","indexes":[0,6,7,1,2,3,5,4,8]}},"star":{"/":{"path":"/star/","indexes":[0]}},"timeline":{"/":{"path":"/timeline/","indexes":[6,7,1,2,3,5,4,8,0]}}};
       
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept();
-  if (__VUE_HMR_RUNTIME__.updateBlogType)
-    __VUE_HMR_RUNTIME__.updateBlogType(typesMap);
-}
-
-if (import.meta.hot)
-  import.meta.hot.accept(({ typesMap }) => {
-    __VUE_HMR_RUNTIME__.updateBlogType(typesMap);
-  });
-
       
--- src/.vuepress/.temp/pages/journal/index.html.js
+++ src/.vuepress/.temp/pages/journal/index.html.js
@@ -1,16 +1,3 @@
 import comp from "/Users/wucheng/code/openspace/wuxiaobai137.github.io/src/.vuepress/.temp/pages/journal/index.html.vue"
-const data = JSON.parse("{\"path\":\"/journal/\",\"title\":\"Journal\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"Journal\",\"article\":false,\"feed\":false,\"sitemap\":false,\"gitInclude\":[],\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://mister-hope.github.io/journal/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"博客演示\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Journal\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"article:author\",\"content\":\"Mr.Hope\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"WebPage\\\",\\\"name\\\":\\\"Journal\\\"}\"]]},\"headers\":[],\"readingTime\":{\"minutes\":0,\"words\":1},\"filePathRelative\":null,\"excerpt\":\"\"}")
+const data = JSON.parse("{\"path\":\"/journal/\",\"title\":\"Journal\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"Journal\",\"article\":false,\"feed\":false,\"sitemap\":false,\"gitInclude\":[],\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://mister-hope.github.io/journal/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"博客演示\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Journal\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"article:author\",\"content\":\"Mr.Hope\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"WebPage\\\",\\\"name\\\":\\\"Journal\\\"}\"]]},\"headers\":[],\"git\":{},\"readingTime\":{\"minutes\":0,\"words\":1},\"filePathRelative\":null,\"excerpt\":\"\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
